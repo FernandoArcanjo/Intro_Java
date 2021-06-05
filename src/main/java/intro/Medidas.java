26d82bb1c9501432cf25c1179368bdc120778375
Fernando Henrique Arcanjo Aparecido <fernando.arcanjo.aparecido@hotmail.com>
2021-06-05 13:14:20 -0300

    Criação de novos métodos de contagem: - contarAteDez - contagemRegressiva
    Além de mostrar como utilizar a calasse Scanner para ler o que o usuário escolheu no teclado.

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -3,19 +3,83 @@ package intro;
 
 // 2 - Referencia as bibliotecas
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 // 3 - Classe
 public class Medidas {
     // 3.1 - Atributos - Características
 
     // 3.2 - Métodos e Funções
     public static void main(String[] args){
+        // Condicional = Verificar uma condição - Fazer uma pergunta para uma pessoa, um hardware ou software
 
-        calcularAreaModoCutro();
-        calcularAreaModoExtenso();
+        // Utilizar a classe Scanner do Java para ler a escolha do usuário no console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("M E N U   D E   O P C O E S ");
+        System.out.println("c - Calcular Area Modo Curto");
+        System.out.println("d - Contar até Dez");
+        System.out.println("e - Calcular Area Modo Extenso");
+        System.out.println("i - If Simples");
+        System.out.println("r - Contagem regressiva de 10 a 0");
+        System.out.println("Digite a opcao desejada: ");
+        String opcao = scanner.next();
+
+
+        // switch = selecionar o comportamento do programa conforme a escolha da pessoa ou do software.
+
+        //String opcao = "curto";
+
+        switch (opcao){
+            case "c":
+            case "C":
+                System.out.println("Você escolheu executar o método calcularAreaModoCurto.");
+                calcularAreaModoCurto();
+                break;
+            case "d":
+            case "D":
+                System.out.println("Você escolheu executar o método contarAteDez");
+                contarAteDez();
+                break;
+            case "e":
+            case "E":
+                System.out.println("Você escolheu executar o método calcularAreaModoExtenso.");
+                calcularAreaModoExtenso();
+                break;
+            case "i":
+            case "I":
+                System.out.println("Você escolheu executar o método ifSimples.");
+                ifSimples();
+                break;
+            case "r":
+            case "R":
+                System.out.println("Você escolheu executar o método contagemRegressiva.");
+                contagemRegressiva();
+                break;
+            default:
+                System.out.println("Você escolheu outro valor que não tem uma ação associada.");
+                break;
+        }
 
     }
 
-    public static void calcularAreaModoCutro(){
+    public static void ifSimples(){
+        // Condicional = Verificar uma condição - Fazer uma pergunta para uma pessoa, um hardware ou software
+
+        // if = se
+        // else = senão
+
+        String modo = "curto";
+
+        if (modo == "curto"){
+            calcularAreaModoCurto();
+        }
+        else{
+            calcularAreaModoExtenso();
+        }
+    }
+
+    public static void calcularAreaModoCurto(){
         System.out.println("Calculo de Areas Modo Curto");
 
         // Calculo de área = Reduzido
@@ -42,5 +106,21 @@ public class Medidas {
         System.out.println("Para a largura de " + largura + ", e comprimento de "
                 + comprimento + ", a área do tapete é de " + resultado + " m².");
     }
+    public static void contarAteDez(){
 
+        // Loops ou Repetições
+        // for = repetição incondicional
+        System.out.println("Contagem Crescente");
+
+        for(int numero = 1; numero <= 10; numero++){
+            System.out.println(numero);
+        }
+    }
+    public static void contagemRegressiva(){
+        System.out.println("Contagem Regressiva");
+
+        for(int numero = 10; numero > -1; numero--){
+            System.out.println(numero);
+        }
+    }
 }
